ed397902b4cf4b8091be8ed4ec0d825f692de3ef
Alex Alexeyev <aasmc@yandex.ru>
2023-08-13 22:16:23 +0300

    add review-service impl and product composite service impl

---
+++ api/src/main/java/ru/aasmc/api/composite/product/ProductCompositeService.java
@@ -0,0 +1,20 @@
+package ru.aasmc.api.composite.product;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public interface ProductCompositeService {
+
+    /**
+     * Sample usage: "curl $HOST:$PORT/product-composite/1".
+     *
+     * @param productId Id of the product
+     * @return the composite product info, if found, else null
+     */
+    @GetMapping(
+            value = "/product-composite/{productId}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    ProductAggregate getProduct(@PathVariable int productId);
+
+}
---
+++ microservices/product-composite-service/src/main/java/ru/aasmc/microservices/composite/product/services/ProductCompositeServiceImpl.java
@@ -0,0 +1,62 @@
+package ru.aasmc.microservices.composite.product.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import ru.aasmc.api.composite.product.*;
+import ru.aasmc.api.core.product.Product;
+import ru.aasmc.api.core.recommendation.Recommendation;
+import ru.aasmc.api.core.review.Review;
+import ru.aasmc.api.exceptions.NotFoundException;
+import ru.aasmc.util.http.ServiceUtil;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProductCompositeServiceImpl implements ProductCompositeService {
+
+    private final ServiceUtil serviceUtil;
+    private final ProductCompositeIntegration integration;
+
+    @Override
+    public ProductAggregate getProduct(int productId) {
+        Product product = integration.getProduct(productId);
+        if (product == null) {
+            throw new NotFoundException("No product found for productId: " + productId);
+        }
+
+        List<Recommendation> recommendations = integration.getRecommendations(productId);
+
+        List<Review> reviews = integration.getReviews(productId);
+
+        return createProductAggregate(product, recommendations, reviews, serviceUtil.getServiceAddress());
+    }
+
+    private ProductAggregate createProductAggregate(
+            Product product,
+            List<Recommendation> recommendations,
+            List<Review> reviews,
+            String serviceAddress) {
+        // 1. Setup product info
+        int productId = product.getProductId();
+        String name = product.getName();
+        int weight = product.getWeight();
+
+        // 2. Copy summary recommendation info, if available
+        List<RecommendationSummary> recommendationSummaries = (recommendations == null) ? null : recommendations.stream()
+                .map(r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(), r.getRate()))
+                .toList();
+
+        // 3. Copy summary review info, if available
+        List<ReviewSummary> reviewSummaries =
+                (reviews == null) ? null : reviews.stream()
+                        .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
+                        .toList();
+        // 4. Create info regarding the involved microservices addresses
+        String productAddress = product.getServiceAddress();
+        String reviewAddress = (reviews != null && reviews.size() > 0) ? reviews.get(0).getServiceAddress() : "";
+        String recommendationAddress = (recommendations != null && recommendations.size() > 0) ? recommendations.get(0).getServiceAddress() : "";
+        ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress, reviewAddress, recommendationAddress);
+        return new ProductAggregate(productId, name, weight, recommendationSummaries, reviewSummaries, serviceAddresses);
+    }
+}
