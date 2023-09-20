c375b65a3f7743915e30ffe861f8f2eae9ca6a64
aleksas <test@test.com>
2023-09-20 17:28:15 +0300

    add security to eureka server

--- spring-cloud/eureka-server/src/test/java/ru/aasmc/springcloud/eurekaserver/EurekaServerApplicationTests.java
+++ spring-cloud/eureka-server/src/test/java/ru/aasmc/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -2,6 +2,7 @@ package ru.aasmc.springcloud.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
@@ -17,8 +18,18 @@ public class EurekaServerApplicationTests {
 
     }
 
-    @Autowired
+    @Value("${app.eureka-username}")
+    private String username;
+
+    @Value("${app.eureka-password}")
+    private String password;
+
     private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    void setTestRestTemplate(TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate.withBasicAuth(username, password);
+    }
 
     @Test
     void catalogLoads() {
